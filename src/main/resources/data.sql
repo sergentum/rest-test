16f9562ae2f414e9bdbb05f64e9dc313dd6fc68b
sergentum <sergentums@gmail.com>
2020-10-14 14:01:56 +0300

    example data script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.product (id, date, name, price) VALUES (1, '2020-10-13 17:14:48.472659', 'Bread', 10);
+INSERT INTO public.product (id, date, name, price) VALUES (2, '2020-10-13 17:14:51.571260', 'Meat', 20);
+INSERT INTO public.product (id, date, name, price) VALUES (3, '2020-10-13 17:34:55.514783', 'Cheese', 15);
